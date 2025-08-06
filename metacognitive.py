14107e57a18df737128413705510d6ac025717fd
Charles C <>
2025-08-06 10:14:52 -0700

    Add problem importance - data structure for response values - prompt and parsing of problem importance - add base for computing the vector value and example implementation - fix bug with NRCLex sometimes returning "anticip"]

--- metacognitive.py
+++ metacognitive.py
@@ -1,10 +1,16 @@
 
+from abc import abstractmethod
 import asyncio
 from dataclasses import dataclass
 import json
 
 from nrclex import NRCLex
 import ollama
+
+@dataclass
+class ResponseVectors:
+    @abstractmethod
+    def compute_value(self) -> int: ...
 
 @dataclass(kw_only=True)
 class EmotionalResponse:
@@ -26,12 +32,29 @@ class CorrectnessResponse:
     contextual_appropriateness: float
 
 @dataclass(kw_only=True)
+class ProblemImportance(ResponseVectors):
+    potential_consequences: float
+    weight_potential_consequences: float = 0.3
+
+    temporal_urgency: float
+    weight_temporal_urgency: float = 0.3
+
+    scope_of_impact: float
+    weight_scope_of_impact: float = 0.3
+
+    def compute_value(self) -> int:
+        return min(int(
+                (self.potential_consequences * self.weight_potential_consequences) + 
+                (self.temporal_urgency * self.weight_temporal_urgency) + 
+                (self.scope_of_impact * self.weight_scope_of_impact)), 100)
+
+@dataclass(kw_only=True)
 class MetacognitiveVector:
     emotional_response: EmotionalResponse
     correctness: CorrectnessResponse
     experiential_matching: int
     conflict_information: int
-    problem_importance: int
+    problem_importance: ProblemImportance
 
     def should_engage_system_two(self) -> bool:
         return False
@@ -52,8 +75,14 @@ async def compute_metacognitive_state_vector(response: str, original_prompt: str
 
 async def _compute_emotional_response(message: str) -> EmotionalResponse:
     text_object = NRCLex(message)
-    # remove vestigial(?) "anticip" in favor of the populated "anticipation"
-    del text_object.affect_frequencies["anticip"]
+    # remove vestigial(?) "anticip" in favor of the populated "anticipation",
+    # seems like sometimes "anticipation" is populated sometimes "anticip" ?
+    if "anticipation" not in text_object.affect_frequencies:
+        if "anticip" in text_object.affect_frequencies:
+            text_object.affect_frequencies["anticipation"] = text_object.affect_frequencies["anticip"]
+            del text_object.affect_frequencies["anticip"]
+        else:
+            text_object.affect_frequencies["anticipation"] = 0.0
     return EmotionalResponse(**text_object.affect_frequencies)
 
 
@@ -78,5 +107,14 @@ async def _compute_experiential_matching(message:str) -> int:
 async def _compute_conflict_information(message:str) -> int:
     return 0
 
-async def _compute_problem_importance(message:str) -> int:
-    return 0
+async def _compute_problem_importance(original_prompt:str) -> ProblemImportance:
+    content = f"""How would you assess the User Prompt for problem importance on the dimensions of potential consequences, temporal urgency, and scope of impact? 
+Assess each dimension from 0 to 100 and return the response in JSON format {{"potential_consequences": "potential consequences", "temporal_urgency": "temporal urgency", "scope_of_impact": "scope of impact"}}, 
+do not include any additional text.
+User Prompt: {original_prompt}"""
+    response = ollama.chat(model="llama3.2", messages=[{"role": "user", "content":content}])
+    try:
+        parsed_response = json.loads(response.message.content)
+        return ProblemImportance(potential_consequences=float(parsed_response["potential_consequences"]), temporal_urgency=float(parsed_response["temporal_urgency"]), scope_of_impact=float(parsed_response["scope_of_impact"]))
+    except:
+        return ProblemImportance(potential_consequences=0.0, temporal_urgency=0.0, scope_of_impact=0.0)
