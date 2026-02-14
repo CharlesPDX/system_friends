b5887db4e6f617ede371a10198a0484752480618
Charles C <>
2026-02-14 15:14:25 -0800

    Move node role enum to common

--- common.py
+++ common.py
@@ -1,4 +1,4 @@
-from enum import StrEnum
+from enum import StrEnum, auto
 
 
 class MetacognitiveComponentNames(StrEnum):
@@ -7,3 +7,12 @@ class MetacognitiveComponentNames(StrEnum):
     Experiential_Matching = "experiential_matching"
     Conflicting_Information = "conflicting_information"
     Problem_Importance = "problem_importance"
+
+
+class NodeRole(StrEnum):
+    Domain_Expert = auto()
+    Critic = auto()
+    Evaluator = auto()
+    Generalist = auto()
+    Synthesizer = auto()
+    System_One = auto()
